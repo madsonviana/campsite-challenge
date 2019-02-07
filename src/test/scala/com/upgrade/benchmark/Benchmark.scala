3755c318d06faa853b3e9387445a1bb4879f73fe
Madson Viana <me@madsonviana.info>
2019-02-07 06:45:26 -0200

    Example of benchmark tests

--- src/test/scala/com/upgrade/benchmark/Benchmark.scala
+++ src/test/scala/com/upgrade/benchmark/Benchmark.scala
@@ -11,7 +11,7 @@ class Benchmark extends Simulation {
   val scn = scenario("FindNotExistingReservation").repeat(10000, "counter") {
     exec(
       http("Find by id")
-        .get(s"http://localhost:8080/reservations/1")
+        .get("http://localhost:8080/server/5c5a198b3acf52388d17c8e1")
         .check(status.is(404))
     )
   }
