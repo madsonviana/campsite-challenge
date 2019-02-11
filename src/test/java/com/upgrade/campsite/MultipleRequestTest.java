1d789763fdec15fd5c0dd8eb1cb484f359cea920
Madson Wlisses Viana <madson.viana@bcnet.bcb.gov.br>
2019-02-11 14:30:40 -0200

    Add multithread booking tests

--- src/test/java/com/upgrade/campsite/MultipleRequestTest.java
+++ src/test/java/com/upgrade/campsite/MultipleRequestTest.java
@@ -1,59 +1,85 @@
 package com.upgrade.campsite;
 
-import com.upgrade.campsite.controller.ReservationController;
 import com.upgrade.campsite.model.Reservation;
-import com.upgrade.campsite.repository.ReservationRepository;
-import com.upgrade.campsite.service.ReservationService;
-import org.junit.Before;
+import com.upgrade.campsite.util.QueueUtil;
+import lombok.Data;
+import org.apache.activemq.command.ActiveMQObjectMessage;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.stream.Stream;
 
+/**
+ * Tests of concurrency booking
+ *
+ * @author Madson Viana
+ */
 @RunWith(SpringRunner.class)
-@WebFluxTest
+@SpringBootTest
 public class MultipleRequestTest {
 
-    @Autowired
-    private ReservationService reservationService;
+    private static final int BOOKING_QUANTITY = 1000;
 
     @Autowired
-    private ReservationRepository reservationRepository;
+    private JmsTemplate jmsTemplate;
 
-    private Reservation reservation;
-
-    @Autowired
-    private WebTestClient webTestClient;
-
-    @Before
-    public void setup() {
-        reservation = Reservation.builder()
+    public Reservation getNewReservation() {
+        return Reservation.builder()
                 .fullName("Madson Viana")
                 .email("me@madsonviana.info")
                 .arrivalDate(LocalDate.now().plusDays(1))
-                .departureDate(LocalDate.now().plusMonths(2))
+                .departureDate(LocalDate.now().plusDays(2))
                 .build();
     }
 
     @Test
-    public void multipleRequestTest() {
+    public void multiplBooking() {
+        final Results results = new Results();
+
         Stream
-                .iterate(reservation, r -> r)
-                .limit(1000)
+                .iterate(getNewReservation(), r -> getNewReservation())
+                .limit(BOOKING_QUANTITY)
                 .parallel()
-                .forEach(r ->
-                    webTestClient.post()
-                            .uri("/api/reservations")
-                            .body(BodyInserters.fromObject(r))
-                );
+                .forEach(r -> {
+                    Message message = jmsTemplate.sendAndReceive(QueueUtil.BOOKING_QUEUE,
+                            session -> QueueUtil.createObjectMessage(session, r));
+                    Serializable response = null;
+                    try {
+                        response = ((ActiveMQObjectMessage) message).getObject();
+                    } catch (JMSException e) { throw new RuntimeException(e); }
+
+                    if (response instanceof Throwable) {
+                        results.addError();
+                    } else {
+                        results.addSuccess();
+                    }
+                });
+
+        Assert.assertEquals(1, results.getSuccess());
+        Assert.assertEquals(BOOKING_QUANTITY - 1, results.getError());
+    }
+
+    @Data
+    private class Results {
+        private int success = 0;
+        private int error = 0;
+
+        synchronized void addSuccess() {
+            success++;
+        }
+
+        synchronized void addError() {
+            error++;
+        }
     }
 
 }
